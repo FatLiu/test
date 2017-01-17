f99d15fb44de9a6d4630edac313ec340899045ef
刘超 <1286323677@qq.com>
2017-01-17 18:22:25 +0800

    'uek'

---
+++ uek/js/rem.js
@@ -0,0 +1,7 @@
+var dw = 750;
+function resize(){
+    var w = document.documentElement.clientWidth; 
+    document.documentElement.style.fontSize= w/dw*100+"px";
+}
+resize();
+window.onresize=resize;
